7fed7ed426361e0a8112f3cc6a180789bc6e2d4a
David Dvořák <dvorakdavid7@gmail.com>
2024-06-06 16:37:54 +0200

    first page

---
+++ landing-page-fe/components/ReservationButton.vue
@@ -0,0 +1,14 @@
+<template>
+    <button class="text-white uppercase p-3 rounded-2xl text-2xl">
+        rezervovat
+    </button>
+</template>
+
+<style scoped>
+button {
+    background-color: var(--primary-red);
+}
+</style>
+
+<script setup lang="ts">
+</script>
--- landing-page-fe/pages/index.vue
+++ landing-page-fe/pages/index.vue
@@ -1,5 +1,34 @@
 <template>
-    <h1 class="text-3xl font-bold underline">
-        Hello world!
-    </h1>
+    <div class="bg-cover bg-center bg-no-repeat h-screen w-screen flex flex-col justify-center items-center">
+        <img src="../assets/logo_white.svg" alt="logo">
+
+        <div class="mt-24 mb-24 text-center">
+            <h1 class="mb-5 text-white text-4xl uppercase">
+                žhavá letní nabíka
+            </h1>
+            <h1 class="text-white text-6xl">
+                <span class="text-primary-red">Plnění klimatizací</span> za
+            </h1>
+            <h1 class="text-white text-6xl">
+                zvýhodněnou cenu!
+            </h1>
+        </div>
+
+        <div>
+            <ReservationButton/>
+        </div>
+
+        <div class="mt-20">
+            <i class="bi bi-chevron-compact-down text-gray-400 text-4xl"></i>
+        </div>
+    </div>
 </template>
+
+<style scoped>
+.bg-cover {
+    background-image: url("../assets/bg_immage_1.svg");
+}
+</style>
+
+<script setup lang="ts">
+</script>
