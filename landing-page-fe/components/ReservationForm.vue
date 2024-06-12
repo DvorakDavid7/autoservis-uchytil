d387518d9c63a3763adffad15adf9dc3459df047
David Dvořák <dvorakdavid7@gmail.com>
2024-06-12 18:29:33 +0200

    add build

--- landing-page-fe/components/ReservationForm.vue
+++ landing-page-fe/components/ReservationForm.vue
@@ -82,7 +82,7 @@
             <textarea
                 id="message"
                 rows="4"
-                class="block p-2.5 w-full text-sm text-gray-900 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+                class="block p-2.5 w-full text-gray-900 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
                 placeholder="Váš text..."
             />
         </div>
---
+++ public_html/_nuxt/DYMRdfKb.js
@@ -0,0 +1,1 @@
+import{aq as r}from"./BTIkOTE_.js";var e=r();export{e as O};
