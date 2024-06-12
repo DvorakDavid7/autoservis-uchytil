655f16f462f78b0e3a2a3aa4b7d95eece28a2808
David Dvořák <dvorakdavid7@gmail.com>
2024-06-12 17:58:04 +0200

    services responsivnes

--- landing-page-fe/components/ServiceList.vue
+++ landing-page-fe/components/ServiceList.vue
@@ -1,5 +1,5 @@
 <template>
-    <section id="services" class="m-0">
+    <section id="services" class="">
         <div class="flex items-center justify-center">
             <div class="mt-10 mb-10 min-h-full flex flex-col items-center">
                 <Headings
@@ -7,35 +7,35 @@
                     headings-title-black=" služby"
                     headings-description="S dvacetiletou praxí"
                 />
-                <div class="grid lg:grid-cols-3 gap-5 md:grid-cols-1">
+                <div class="grid lg:grid-cols-3 gap-5 md:grid-cols-2 sm:grid-cols-1 mt-20">
                     <Service
                         service-title="Plnění klimatizací"
-                        service-image="_nuxt/assets/services/service_climatization.svg"
+                        service-image="../assets/services/service_climatization.svg"
                         service-description="Plnění klimatizace do aut je klíčovým prvkem udržení optimálního komfortu a bezpečnosti během jízdy. "
                     />
                     <Service
                         service-title="Oprava automobilu"
-                        service-image="_nuxt/assets/services/service_car.svg"
+                        service-image="../assets/services/service_car.svg"
                         service-description="Provádíme diagnostiku, opravy motoru, brzdového systému, drobné elektroniky, převodovky a dalších klíčových částí."
                     />
                     <Service
                         service-title="Příprava na STK"
-                        service-image="_nuxt/assets/services/service_stk.svg"
+                        service-image="../assets/services/service_stk.svg"
                         service-description="Příprava auto na STK je klíčovou službou, která zajistí, že vaše vozidlo splňuje veškeré technické požadavky pro úspěšnou kontrolu."
                     />
                     <Service
                         service-title="Výměna kapalin"
-                        service-image="_nuxt/assets/services/service_fluids.svg"
+                        service-image="../assets/services/service_fluids.svg"
                         service-description="Tato komplexní služba zahrnuje výměnu motorového oleje, chladící kapaliny, brzdové kapaliny, převodového oleje a dalších provozních kapalin."
                     />
                     <Service
                         service-title="Přezutí kol"
-                        service-image="_nuxt/assets/services/service_tire.svg"
+                        service-image="../assets/services/service_tire.svg"
                         service-description="Přezutí a uskladnění kol včetně vyvážení jsou další z našich klíčových služb našeho autoservisu, které přinášejí pohodlí a bezpečnost."
                     />
                     <Service
                         service-title="Uskladnění kol"
-                        service-image="_nuxt/assets/services/service_garage.svg"
+                        service-image="../assets/services/service_garage.svg"
                         service-description="Nabízíme uskladnění kol. Naše bezpečné a klimatizované sklady zajistí, že vaše pneumatiky budou chráněny před nepříznivými vlivy počasí."
                     />
                 </div>
