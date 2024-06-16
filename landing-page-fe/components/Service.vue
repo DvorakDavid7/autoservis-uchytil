813c9c01804e76d4f4eaa47db7737f239f1b70bd
David Dvořák <dvorakdavid7@gmail.com>
2024-06-16 11:47:30 +0200

    service highlight

--- landing-page-fe/components/Service.vue
+++ landing-page-fe/components/Service.vue
@@ -7,14 +7,19 @@
                     <img :src="serviceImage" alt="Service Image"/>
                 </div>
             </div>
-            <Card style="max-width: 25rem;" class="justify-center mb-16 shadow items-center">
+            <Card style="max-width: 25rem;"
+                  :class="`justify-center mb-16 shadow items-center ${highlighted && 'bg-[var(--primary-red)] scale-105'}`"
+            >
                 <template #title>
-                    <div class=" mt-2 uppercase text-black text-2xl">
+                    <div :class="`mt-2 uppercase text-2xl ${textColor}`">
                         <h1>{{ serviceTitle }}</h1>
                     </div>
                 </template>
                 <template #content>
-                    <p class="m-0 hyphens-autotext-balance text-gray-400 text-md" lang="cz">
+                    <p
+                        :class="`m-0 hyphens-autotext-balance text-gray-400 text-md ${textColor}`"
+                        lang="cz"
+                    >
                         {{ serviceDescription }}
                     </p>
                 </template>
@@ -30,25 +35,16 @@ img {
 }
 </style>
 
-<script lang="ts">
+<script setup lang="ts">
 
-import {defineComponent} from 'vue'
+interface ServiceProps {
+    serviceTitle: string
+    serviceDescription: string
+    serviceImage: string
+    highlighted?: boolean
+}
 
-export default defineComponent({
-    name: 'Service',
-    props: {
-        serviceImage: {
-            type: String,
-            required: true
-        },
-        serviceTitle: {
-            type: String,
-            required: true
-        },
-        serviceDescription: {
-            type: String,
-            required: true
-        }
-    }
-})
+const textColor = computed(() => highlighted ? "text-white" : "text-black")
+
+const {serviceTitle, serviceImage, serviceDescription, highlighted} = defineProps<ServiceProps>()
 </script>
