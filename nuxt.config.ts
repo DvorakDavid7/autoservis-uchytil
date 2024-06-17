e3ff4233dff78a88f712657d965f8defe85453a4
David Dvořák <dvorakdavid7@gmail.com>
2024-06-17 10:40:51 +0200

    time picker inline google analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 
-  devtools: { enabled: true },
-  css: ["~/assets/css/main.css"],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
+    devtools: {enabled: true},
+    css: ["~/assets/css/main.css"],
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
     },
-  },
-  plugins: [
-    '@/plugins/counter.client.ts'
-  ],
-  modules: [
-    'nuxt-primevue',
-  ]
+    plugins: [
+        "@/plugins/counter.client.ts"
+    ],
+    modules: ["nuxt-primevue", "nuxt-gtag"],
+    gtag: {
+        id: "G-14KXHZFJ8L"
+    }
 })
