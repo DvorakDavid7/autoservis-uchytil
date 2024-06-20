2c78d21d4f44ef82ce8ed094fcd4d5a86fd2e770
Jan Kejř <jan.kejr@centrum.cz>
2024-06-21 00:39:42 +0200

    feat(cookies): add cookies page

---
+++ components/Cookies.vue
@@ -0,0 +1,156 @@
+<template>
+  <section id="personal-data" class="">
+      <div class="flex items-center justify-center">
+          <div class="mt-24 mb-10 min-h-full flex flex-col items-center">
+              <Headings
+                  headings-title="Zásady"
+                  headings-title-black="Cookies"
+                  headings-description=""
+              />
+
+              <div class="text-block text-justify">
+                <p>Tyto Zásady cookies byly naposledy aktualizovány 20. 6. 2024 a vztahují se na 
+                    občany a osoby s trvalým pobytem v Evropském hospodářském prostoru.</p>
+                <br/>
+                <h2 class="text-left text-2xl leading-loose">1. Úvod</h2>
+                <p>Naše webové stránky https://autoservisjju.cz (dále jen „web“) používají cookies a další související technologie 
+                    (pro usnadnění jsou všechny technologie označovány jako „cookies“). Cookies také vkládají třetí strany, 
+                    které jsme zapojili. V níže uvedeném dokumentu vás informujeme o používání cookies na našem webu.</p>
+              </div>
+              <div class="text-block">
+                  <h2 class="text-left text-2xl leading-loose">2. Co jsou soubory cookies?</h2>
+                  <p>Soubor cookie je malý jednoduchý soubor, který je odeslán spolu se stránkami tohoto webu a uložen prohlížečem na pevný disk počítače 
+                      nebo jiného zařízení. Informace v nich uložené mohou být vráceny našim serverům nebo serverům příslušných třetích stran během následné návštěvy.</p>
+              </div>
+              <div class="text-block">
+                  <h2 class="text-left text-2xl leading-loose">3. Co jsou skripty</h2>
+                  <p>Skript je část programového kódu, který slouží k tomu, aby naše webové stránky fungovaly správně a interaktivně. 
+                      Tento kód je spuštěn na našem serveru nebo na vašem zařízení.</p>
+              </div>
+              <div class="text-block text-justify">
+                  <h2 class="text-left text-2xl leading-loose">4. Co je to webový maják?</h2>
+                  <p>Webový maják (nebo pixelová značka) je malý, neviditelný kus textu nebo obrázku na webu, 
+                      který se používá ke sledování provozu na webu. Za tímto účelem jsou různá data o vás ukládána pomocí webových majáků.</p>
+              </div>
+              <div class="text-block text-justify">
+                  <h2 class="text-left text-2xl leading-loose">5. Cookies</h2>
+                  <p><strong>Technické nebo funkční soubory cookies</strong></p>
+                  <p>Některé soubory cookies zajišťují, že určité části webu fungují správně a že vaše uživatelské preference zůstávají známé. 
+                      Umístěním funkčních souborů cookies usnadňujeme návštěvu našich webových stránek. Tímto způsobem nemusíte při návštěvě našich webových stránek opakovaně 
+                      zadávat stejné informace a například položky zůstanou v nákupním košíku, dokud nezaplatíte. Tyto cookies můžeme umístit bez vašeho souhlasu.</p>
+                  <br>
+                  <p><strong>Statistické cookies</strong></p>
+                  <p>Statistické soubory cookies využíváme k optimalizaci webových stránek pro naše uživatele. 
+                      Díky těmto statistickým cookies získáváme přehled o používání našich webových stránek. 
+                      Žádáme vás o povolení k ukládání statistických souborů cookies.</p>
+                  <br>
+                  <p><strong>Marketingové/Sledovací cookies</strong></p>
+                  <p>Marketingové/sledovací cookies jsou soubory cookies nebo jakákoli jiná forma místního úložiště, 
+                      které se používají k vytváření uživatelských profilů k zobrazování reklamy nebo ke sledování uživatele na tomto webu 
+                      nebo na několika webech pro podobné marketingové účely.</p>
+              </div>
+              <div class="text-block text-justify">
+                  <h2 class="text-left text-2xl leading-loose">6. Souhlas</h2>
+                  <p>Když poprvé navštívíte náš web, ukážeme vám vyskakovací okno s vysvětlením o cookies. 
+                      Jakmile kliknete na „Uložit předvolby“, vyjadřujete svůj souhlas s používáním kategorií 
+                      souborů cookies a doplňků popsaných ve vyskakovacím okně a v těchto Zásadách cookies. 
+                      Používání cookies můžete zakázat pomocí svého prohlížeče, ale mějte na paměti, že naše webové stránky již nemusí fungovat správně.</p>
+              </div>
+              <div class="text-block text-justify">
+                  <h2 class="text-left text-2xl leading-loose">6. Umístěné cookies</h2>
+                  <div class="relative overflow-x-auto">
+                    <table class="w-full text-sm text-left rtl:text-right text-gray-800 dark:text-white">
+                        <thead class="text-xs text-white uppercase bg-gray-50 dark:bg-gray-300 dark:text-gray-800">
+                            <tr>
+                                <th scope="col" class="px-6 py-3">
+                                    Název
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Expirace
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Funkce
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    _ga_*
+                                </th>
+                                <td class="px-6 py-4">
+                                    1 rok
+                                </td>
+                                <td class="px-6 py-4">
+                                    Uchovávat a počítat zobrazení stránek
+                                </td>
+                            </tr>
+                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    _ga
+                                </th>
+                                <td class="px-6 py-4">
+                                    2 roky
+                                </td>
+                                <td class="px-6 py-4">
+                                    Uchovávat a počítat zobrazení stránek
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+              </div>
+              <div class="text-block text-justify">
+                  <h2 class="text-left text-2xl leading-loose">8. Povolení/zakázání a odstranění cookies</h2>
+                  <p>Pomocí internetového prohlížeče můžete automaticky nebo ručně mazat soubory cookies. Můžete také určit, že některé soubory cookies nemusí být umístěny. 
+                      Další možností je změnit nastavení internetového prohlížeče tak, aby se vám při každém uložení souboru cookies zobrazila zpráva. 
+                      Další informace o těchto možnostech naleznete v Nápovědě vašeho prohlížeče.</p>
+                  <br>
+                  <p>Vezměte prosím na vědomí, že náš web nemusí fungovat správně, 
+                      pokud jsou deaktivovány všechny cookies. Pokud cookies smažete ve svém prohlížeči, 
+                      budou znovu umístěny po vašem souhlasu, když znovu navštívíte naše webové stránky.</p>
+              </div>
+              <div class="text-block text-justify">
+                  <h2 class="text-left text-2xl leading-loose">9. Vaše práva týkající se osobních údajů</h2>
+                  <p>Pokud jde o vaše osobní údaje, máte následující práva:</p>
+                    <ol class="list-disc">
+                        <li>Máte právo vědět, proč jsou vaše osobní údaje potřebné, co se s nimi stane a jak dlouho budou uchovány.</li>
+                        <li>Právo na přístup: Máte právo na přístup k vašim osobním údajům, které jsou nám známy.</li>
+                        <li>Právo na opravu: Máte právo doplnit, opravit, odstranit nebo zablokovat vaše osobní údaje, kdykoli budete chtít.</li>
+                        <li>Pokud nám udělíte souhlas se zpracováním vašich údajů, máte právo tento souhlas odvolat a nechat vaše osobní údaje smazat.</li>
+                        <li>Právo na přenos vašich údajů: Máte právo vyžádat si od správce všechny vaše osobní údaje a v celém rozsahu je předat jinému správci.</li>
+                        <li>Právo na námitku: proti zpracování vašich údajů můžete namítat. Řídíme se tím, pokud neexistují oprávněné důvody ke zpracování.</li>
+                    </ol>
+                  <p>Pro uplatnění těchto práv nás prosím kontaktujte. Máte-li stížnost na to, jak nakládáme s vašimi údaji, 
+                      rádi bychom se o tom dozvěděli, ale máte také právo podat stížnost kontrolnímu orgánu (Úřadu pro ochranu osobních údajů).</p>
+              </div>
+          </div>
+      </div>
+  </section>
+</template>
+
+<style scoped>
+
+.text-block {
+  background-color: #fff;
+  padding: 10px;
+  margin: 10px auto;
+  width: 80%;
+  max-width: 1200px;
+  border-radius: 8px;
+}
+
+.text-block p {
+  margin: 0;
+  line-height: 1.6;
+}
+
+.text-block li {
+  margin: 0 20px;
+  line-height: 1.6;
+}
+
+</style>
+
+<script setup lang="ts">
+</script>
