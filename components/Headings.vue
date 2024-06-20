df383b175e061b1fb6a629dadea0613a9c8856fd
Jan Kejř <jan.kejr@centrum.cz>
2024-06-21 00:31:57 +0200

    feat(headings): add leading and size for responsive design

--- components/Headings.vue
+++ components/Headings.vue
@@ -1,13 +1,14 @@
 <template>
-    <div class="mt-10 mb-10 text-center">
-        <h1 class="mb-2 text-[var(--primary-red)] text-2xl sm:text-5xl uppercase">
-            <strong>{{ headingsTitle }}</strong>
-            <span class="relative">
+    <div class="mt-5 mb-5 lg:mt-5 lg:mb-5 text-center">
+        <h1 class="mb-2 text-[var(--primary-red)] text-4xl lg:text-5xl uppercase leading-snug lg:leading-snug">
+        <strong>{{ headingsTitle }}</strong>
+        <br/>
+        <span class="relative">
         <span class="block absolute -inset-1 -skew-x-12 bg-black " aria-hidden="true"></span>
         <span class="relative text-white"><em><strong>{{ headingsTitleBlack }}</strong></em></span>
-      </span>
+        </span>
         </h1>
-        <h3 class="mb-5 mt-5 font-light text-gre text-sm sm:text-xl uppercase">
+        <h3 class="mb-5 mt-5 font-light text-gray-500 text-sm md:text-xl uppercase">
             {{ headingsDescription }}
         </h3>
     </div>
